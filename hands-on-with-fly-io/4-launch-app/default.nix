10cd946dcfeebc9434d27c35a055af1373066eb6
Saikat Das <saikatdas0790@gmail.com>
2023-10-27 16:33:18 +0530

    4. Launch app

---
+++ hands-on-with-fly-io/4-launch-app/default.nix
@@ -0,0 +1,11 @@
+{}:
+let
+  rev = "75a52265bda7fd25e06e3a67dee3f0354e73243c";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+  pkgs = import nixpkgs { };
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    flyctl
+  ];
+}
